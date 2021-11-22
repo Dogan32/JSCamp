fcb48c6112c1b21324dc699de189f2500d703b66
Dogan32 <ozdemirdogannnn@gmail.com>
2021-11-22 23:54:38 +0300

    22 Kasım Ders Sonu

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,4 +1,5 @@
 import { BaseLogger, ElasticLogger, MongoLogger } from "../crossCuttingConcerns/logging/logger.js"
+import Customer from "../models/customer.js"
 import User from "../models/user.js"
 import UserService from "../services/userService.js"
 
@@ -12,10 +13,8 @@ let user2 = new User(2,"Semih","Bekiroğlu","Muş")
 userService.add(user1)
 userService.add(user2)
 
-console.log(userService.list())
-console.log(userService.getById(2))
-
-
+//console.log(userService.list())
+//console.log(userService.getById(2))
 
 let customer = {id:1, firstName:"Doğan"}
 
@@ -23,3 +22,17 @@ let customer = {id:1, firstName:"Doğan"}
 customer.lastName = "Özdemir"
 
 console.log(customer.lastName)
+
+console.log("-----------------------")
+userService.load()
+
+
+let customerToAdd = new Customer(1,"Clara","Özdemir","Bükreş","asdadadsad")
+customerToAdd.type = "customer"
+
+userService.add(customerToAdd)
+
+console.log(userService.customers)
+console.log(userService.employees)
+console.log(userService.errors)
+console.log(userService.getCustomersSorted())
---
+++ src/data/users.js
@@ -0,0 +1,48 @@
+export const users = [
+    {
+        id: 1,
+        firstName: "Doğan",
+        lastName: "Özdemir",
+        city: "İstanbul",
+        age: 29,
+        creditCardNumber: "123456",
+        type: "customer"
+    },
+    {
+        id: 2,
+        firstName: "Semih",
+        lastName: "Bekir",
+        city: "Rize",
+        age: 28,
+        creditCardNumber: "654321",
+        type: "customer"
+    },
+    {
+        id: 3,
+        firstName: "Volkan",
+        lastName: "İrmak",
+        city: "Erzurum",
+        age: 30,
+        salary: 8000,
+        type: "employee"
+
+    },
+    {
+        id: 4,
+        firstName: "Kıvanç",
+        lastName: "Sav",
+        city: "Çorum",
+        age: 22,
+        salary: 8500,
+        type: "employee"
+    },
+    {
+        id: 5,
+        firstName: "Eren",
+        lastName: "Bal",
+        city: "Kars",
+        age: 21,
+        salary: 3500,
+        type: "employee"
+    }
+]
